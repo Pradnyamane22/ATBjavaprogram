a5d3716163653ef1630dd9df13e25a8711fa7a71
Pradnyamane22 <pradnyamane22@gmail.com>
2024-09-15 15:01:42 +0530

    Add files via upload

---
+++ src/sept/exe04/lab004helloworld.java
@@ -0,0 +1,7 @@
+package sept.exe05;
+
+public class lab004helloworld {
+    public static void main(String[] args) {
+        System.out.print("Hello world..!!");
+    }
+}
---
+++ src/sept/exe04/lab007.java
@@ -0,0 +1,14 @@
+package sept.exe05;
+
+public class lab007   {
+    public static void main(String[] args) {
+        //This code will not execute
+        /*  this is multiline comments
+        used for developer instructions
+        you can write anything here
+        multiline
+         */
+// System.out.println("Hello");
+   }
+}
+
